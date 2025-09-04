d95fec7b7de02418b74d46a147ac417489e33086
good0neighbour <notyeas@gmail.com>
2025-09-04 09:56:31 +0900

    2025.09.03

---
+++ python lib/my_module.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# 함수 배운 내용 점검하기.
+# 1. 더하기 -> plus
+def plus(a, b):
+    result = a + b
+    return result
+# 2. 빼기 minus
+def minus(a, b):
+    return a - b
+# 3. 곱하기 multi
+def multi(a, b):
+    return a * b
+# 4. 나누기 divide
+def divide(a, b):
+    return a / b
+
+
+# In[4]:
+
+
+print(plus(1, 2))
+print(minus(3, 0))
+print(multi(2, 3))
+print(divide(4, 3))
+
+
+# In[ ]:
+
+
+
+
